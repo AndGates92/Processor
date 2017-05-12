de77c670ea1cf25bd68dc8087c52a8e3e7226c75
AndGate92 <mrandre.epfl@gmail.com>
2017-05-12 23:31:20 +0100

    name FSM states and assign first signals

--- ddr2_phy_bank_ctrl.vhd
+++ ddr2_phy_bank_ctrl.vhd
@@ -14,75 +14,140 @@ port (
 	clk		: in std_logic;
 
 	-- User Interface
-	ColMemIn	: in std_logic_vector(COL_L - 1 downto 0);
 	RowMemIn	: in std_logic_vector(ROW_L - 1 downto 0);
-	ReadMem		: in std_logic;
-	LastBurst	: in std_logic;
-	UIReq		: in std_logic;
+	CtrlReq		: in std_logic;
 
-	UIAck		: out std_logic;
+	CtrlAck		: out std_logic;
 
 	-- Arbitrer
-	ColMemOut		: out std_logic_vector(COL_L - 1 downto 0);
 	RowMemOut		: out std_logic_vector(ROW_L - 1 downto 0);
 	CmdOut			: out std_logic_vector(CMD_MEM_L - 1 downto 0);
 	CmdReq			: out std_logic;
 
 	CmdAck			: in std_logic;
 
+	-- Controller
+	OutstandingBursts	: out std_logic_vector(MAX_OUTSTANDING_BURSTS_L - 1 downto 0);
+	BankActive		: out std_logic;
+
+	EndDataPhase		: in std_logic;
+	ReadBurst		: in std_logic;
+	LastBurstBeat		: in std_logic
+
 );
 end entity ddr2_phy_bank_ctrl;
 
 architecture rtl of ddr2_phy_bank_ctrl is
+	constant zero_clk_delay	: unsigned(CNT_DELAY_L - 1 downto 0) := (others => '0'); 
+	constant zero_clk_delay	: unsigned(MAX_OUTSTANDING_BURSTS_L - 1 downto 0) := (others => '0'); 
 
-	signal BankActiveC, BankActiveN		: std_logic;
+	signal BankActiveC, BankActiveN	: std_logic;
 
-	signal TActColExceeded		: std_logic;
-	signal TRASExceeded		: std_logic;
-	signal TRPExceeded		: std_logic;
-	signal TRCExceeded		: std_logic;
-	signal TWrPREExceeded		: std_logic;
-	signal TRdPREExceeded		: std_logic;
-	signal TColColExceeded		: std_logic;
-	signal CntBankCtrlC, CntBankCtrlN	: unsigned(CNT_BANK_CTRL_L - 1 downto 0);
-	signal CntDelayC, CntDelayN		: unsigned(CNT_DELAY_L - 1 downto 0);
+	signal TActColReached		: std_logic;
+	signal TRASReached		: std_logic;
+	signal TRCReached		: std_logic;
+
+	signal DelayElapsed		: std_logic;
+
+	signal ExitDataPhase		: std_logic;
+
+	signal CntBankCtrlC, CntBankCtrlN		: unsigned(CNT_BANK_CTRL_L - 1 downto 0);
+	signal CntDelayC, CntDelayN			: unsigned(CNT_DELAY_L - 1 downto 0);
+	signal OutstandingBurstsC, OutstandingBurstsN	: unsigned(MAX_OUTSTANDING_BURSTS_L - 1 downto 0);
 
 	signal StateC, StateN			: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0);
+
+	signal CmdReqC, CmdReqN		: std_logic;
 
 begin
 
 	reg: process(rst, clk)
 	begin
 		if (rst = '1') then
+			BankActiveC <= '0';
 
-			StateC <= START_INIT;
+			CntDelayC <= (others => '0');
+			CntBankCtrlC <= (others => '0');
+			OutstandingBurstC <= (others => '0');
+
+			StateC <= IDLE;
+
+			CmdReqC <= '0';
 
 		elsif ((clk'event) and (clk = '1')) then
+			BankActiveC <= BankActiveN;
+
+			CntDelayC <= CntDelayN;
+			CntBankCtrlC <= CntBankCtrlN;
+			OutstandingBurstC <= OutstandingBurstN;
 
 			StateC <= StateN;
+
+			CmdReqC <= CmdReqN;
 
 		end if;
 	end process reg;
 
+	OutstandingBursts <= std_logic_vector(OutstandingBurstsC);
+	BankActive <= std_logic_vector(BankActiveC);
+	CmdOut <= CMD_BANK_ACT;
+	CmdReq <= CmdReqC;
+	CtrlAck <=	CmdAck when (State = WAIT_ACT_ACK) else -- return ack only when arbitrer gives the ack
+			'1' when ((StateC = ELAPSE_T_ACT_COL) or ((BankActiveC = '1') and  (ExitDataPhase = '0'))) else -- accept immediately the request because the bank is already active or about ot be
+			'0';
 
+	CmdReqN <=	'1' when ((StateC = IDLE) and (CtrlReq = '1')) else
+			'0' when ((StateC = WAIT_ACT_ACK) and (CmdAck = '1')) else
+			CmdReqC;
 
-	TActColExceeded <= not (CntBankCtrlC < to_unsigned(T_ACT_COL));
-	TRASExceeded <= not (CntBankCtrlC < to_unsigned(T_RAS));
-	TRCExceeded <= not (CntBankCtrlC < to_unsigned(T_RC));
-	TWrPREExceeded <= not (CntBankCtrlC < to_unsigned(T_WRITE_PRE));
-	TRdPREExceeded <= not (CntBankCtrlC < to_unsigned(T_READ_PRE));
+	BankActiveN <=	'1' when (TActColReached = '1') else
+			'0' when (ExitDataPhase = '1') else
+			BankActiveC;
 
-	TRPExceeded <= not (CntDelayC < to_unsigned(T_RP));
-	TColColExceeded <= not (CntDelayC < to_unsigned(T_COL_COL));
+	TActColReached <= '0' when (CntBankCtrlC < to_unsigned(T_ACT_COL - 1)) else '1';
+	TRASReached <= '0' when (CntBankCtrlC < to_unsigned(T_RAS - 1)) else '1';
+	TRCReached <= '0' when (CntBankCtrlC < to_unsigned(T_RC - 1)) else '1';
 
-	state_det: process(StateC, UIReq)
+	DelayElapsed <= '1' when (CntDelayC = zero_clk_delay) else '0';
+	NoOutstandingBurstsC <= '1' when (OutstandingBurstsC = zero_outstanding_bursts) else '0';
+
+	ExitDataPhase <= (EndDataPhase = '1') and (NoOutstandingBursts = '1');
+
+	state_det: process(StateC, CtrlReq, CmdAck, TActColReached, EndDataPhase, OutstandingBurstsC, TRASReached, TRCReached, DelayElapsed)
 	begin
 		StateN <= StateC; -- avoid latched
 		if (StateC = IDLE) then
-			if (UIReq = '1') then
+			if (CtrlReq = '1') then
 				StateN <= WAIT_ACT_ACK;
 			end if;
 		elsif (StateC = WAIT_ACT_ACK) then
+			if (CmdAck = '1') begin
+				StateN <= ELAPSE_T_ACT_COL;
+			end if;
+		elsif (StateC = ELAPSE_T_ACT_ROW) then
+			if (TActColReached = '1') begin
+				StateN <= DATA_PHASE;
+			end if;
+		elsif (StateC = DATA_PHASE) then
+			if (ExitDataPhase = '1') begin
+				StateN <= PROCESS_COL_CMD;
+			end if;
+		elsif (StateC = PROCESS_COL_CMD) then
+			if (DelayElapsed = '1') begin
+				if (TRASReached = '1') begin
+					StateN <= ELAPSE_T_RP;
+				else
+					StateN <= ELAPSE_T_RAS;
+				end if;
+			end if;
+		elsif (StateC = ELAPSE_T_RAS) then
+			if (TRASReached = '1') begin
+				StateN <= ELAPSE_T_RP;
+			end if;
+		elsif (StateC = ELAPSE_T_RP) then
+			if ((DelayElapsed = '1') and (TRCReached = '1')) begin
+				StateN <= PROCESS_COL_CMD;
+			end if;
 		end if;
 	end process state_det;
 	
--- ddr2_phy_bank_ctrl_pkg.vhd
+++ ddr2_phy_bank_ctrl_pkg.vhd
@@ -10,21 +10,23 @@ use work.ddr2_pkg.all;
 package ddr2_phy_bank_ctrl_pkg is 
 
 	constant T_ACT_COL	: positive := T_RCD - to_integer(unsigned(AL)); 
-	constant T_READ_PRE	: positive := to_integer(unsigned(AL)) + (2**(to_integer(unsigned(BL)) - 1));
 	constant T_WRITE_PRE	: positive := WRITE_LATENCY + to_integer(unsigned(T_WR)) + (2**(to_integer(unsigned(BL)) - 1));
-	constant T_COL_COL	: positive := 2**(to_integer(unsigned(BL)) - 1);
+	constant T_WRITE_ACT	: positive := T_RP + T_WRITE_PRE;
+	constant T_READ_PRE	: positive := to_integer(unsigned(AL)) + (2**(to_integer(unsigned(BL)) - 1));
+	constant T_READ_ACT	: positive := T_RP + T_READ_PRE;
 
-	constant CNT_BANK_CTRL_L	: integer := int_to_bit_num(max_int(T_RAS, max_int(T_RC, max_int(T_ACT_COL, max_int(T_READ_PRE, T_WRITE_PRE)))));
-	constant CNT_DELAY_L		: integer := int_to_bit_num(max_int(T_RP, T_COL_COL));
+	constant CNT_BANK_CTRL_L	: integer := int_to_bit_num(max_int(T_RAS, max_int(T_RC, T_ACT_COL)));
+	constant CNT_DELAY_L		: integer := int_to_bit_num(max_int(T_READ_PRE, max_int(T_WRITE_PRE, T_RP)));
 
-	constant STATE_BANK_CTRL_L	: positive := 4;
+	constant STATE_BANK_CTRL_L	: positive := 3;
 
-	constant IDLE			: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(0, STATE_BANK_CTRL_L));
-	constant WAIT_ACT_ACK		: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(1, STATE_BANK_CTRL_L));
-	constant WAIT_T_ACT_COL		: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(2, STATE_BANK_CTRL_L));
-	constant WAIT_COL_ACK		: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(3, STATE_BANK_CTRL_L));
-	constant WAIT_PRE_ACT		: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(4, STATE_BANK_CTRL_L));
-	constant WAIT_VALID_DATA	: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(5, STATE_BANK_CTRL_L));
+	constant IDLE				: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(0, STATE_BANK_CTRL_L));
+	constant WAIT_ACT_ACK			: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(1, STATE_BANK_CTRL_L));
+	constant ELAPSE_T_ACT_COL		: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(2, STATE_BANK_CTRL_L));
+	constant DATA_PHASE			: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(3, STATE_BANK_CTRL_L));
+	constant PROCESS_COL_CMD		: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(4, STATE_BANK_CTRL_L));
+	constant ELAPSE_T_RAS			: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(5, STATE_BANK_CTRL_L));
+	constant ELAPSE_T_RP			: std_logic_vector(STATE_BANK_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(6, STATE_BANK_CTRL_L));
 
 	component ddr2_phy_bank_ctrl
 	port (
