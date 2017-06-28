108ffc1808ec3b58c98ad0dfa9fa95eec027f4be
AndGate92 <mrandre.epfl@gmail.com>
2017-06-28 22:47:24 +0100

    remove msr pkg from libraries

--- src/rtl/ddr2_phy_bank_ctrl.vhd
+++ src/rtl/ddr2_phy_bank_ctrl.vhd
@@ -4,7 +4,6 @@ use ieee.numeric_std.all;
 
 library work;
 use work.proc_pkg.all;
-use work.ddr2_mrs_pkg.all;
 use work.ddr2_timing_pkg.all;
 use work.ddr2_phy_pkg.all;
 use work.ddr2_phy_bank_ctrl_pkg.all;
