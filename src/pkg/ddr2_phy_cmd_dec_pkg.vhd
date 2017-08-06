26f30dcd1bf426ab48817903553f5ad0ca51781a
AndGate92 <mrandre.epfl@gmail.com>
2017-08-06 15:29:44 +0100

    Fix syntax error

--- src/pkg/ddr2_phy_cmd_dec_pkg.vhd
+++ src/pkg/ddr2_phy_cmd_dec_pkg.vhd
@@ -8,7 +8,7 @@ use work.proc_pkg.all;
 use work.ddr2_phy_pkg.all;
 use work.ddr2_mrs_pkg.all;
 
-package ddr2_phy_cmd_dec_pkg is 
+package ddr2_phy_cmd_dec_pkg is
 
 	component ddr2_phy_cmd_dec is
 	generic (
@@ -25,7 +25,7 @@ package ddr2_phy_cmd_dec_pkg is
 		RowIn	: in std_logic_vector(ROW_L - 1 downto 0);
 		BankIn	: in std_logic_vector(int_to_bit_num(BANK_NUM) - 1 downto 0);
 		CmdIn	: in std_logic_vector(MEM_CMD_L - 1 downto 0);
-		MRSCmd	: in std_logic_vector(ADDR_L - 1 downto 0)
+		MRSCmd	: in std_logic_vector(ADDR_L - 1 downto 0);
 
 		ClkEnable		: out std_logic;
 		nChipSelect		: out std_logic;
