6ca2cf3a90ec7e0148e11f8c05d7e2b7955d0bae
AndGate92 <mrandre.epfl@gmail.com>
2017-08-13 21:58:34 +0100

    fix T_COL_COL indenting

--- src/pkg/ddr2_phy_col_ctrl_pkg.vhd
+++ src/pkg/ddr2_phy_col_ctrl_pkg.vhd
@@ -11,11 +11,12 @@ use work.ddr2_gen_ac_timing_pkg.all;
 
 package ddr2_phy_col_ctrl_pkg is 
 
+	-- Turn around time
 	constant T_RTW_tat	: positive := 2 + (2**(to_integer(unsigned(BURST_LENGTH)) - 1));
 	constant T_WTR_tat	: positive := to_integer(unsigned(CAS)) - 1 + (2**(to_integer(unsigned(BURST_LENGTH)) - 1)) + T_WTR;
 
-	constant T_COL_COL	: positive := 2**(to_integer(unsigned(BURST_LENGTH)) - 1);
-	constant CNT_COL_TO_COL_L		: positive := int_to_bit_num(T_COL_COL);
+	constant T_COL_COL		: positive := 2**(to_integer(unsigned(BURST_LENGTH)) - 1);
+	constant CNT_COL_TO_COL_L	: positive := int_to_bit_num(T_COL_COL);
 
 	constant CNT_COL_CTRL_L		: integer := int_to_bit_num(max_int(T_RTW_tat, T_WTR_tat));
 
