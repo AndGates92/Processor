8e071ff33f265e8b949d8774cf9cb15a7aca1e7d
AndGate92 <mrandre.epfl@gmail.com>
2017-08-21 05:26:55 +0100

    Add run_arbiter procedure I/O

--- verif/tb/arbiter_tb.vhd
+++ verif/tb/arbiter_tb.vhd
@@ -25,6 +25,8 @@ architecture bench of arbiter_tb is
 	constant MAX_REQUESTS_PER_TEST		: integer := 500;
 	constant MAX_DELAY_PER_REQUEST		: integer := 20;
 
+	constant ZERO_REQ_ACK_VEC	: std_logic_vector(NUM_REQ_TB - 1 downto 0) := (others => '0');
+
 	signal clk_tb	: std_logic := '0';
 	signal stop	: boolean := false;
 	signal rst_tb	: std_logic;
@@ -85,7 +87,7 @@ begin
 			rst_tb <= '0';
 		end procedure reset;
 
-		procedure test_param(variable num_requests : out integer; variable req_delay, data_arr, ack_delay : out int_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (NUM_REQ_TB - 1)); variable stop_arb : out bool_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (NUM_REQ_TB - 1)); variable seed1, seed2: inout positive) is
+		procedure test_param(variable num_requests : out integer; variable req_delay : in int_arr(0 to (MAX_REQUESTS_PER_TEST - 1)); variable data_arr, ack_delay : out int_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (NUM_REQ_TB - 1)); variable req, stop_arb : out bool_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (NUM_REQ_TB - 1)); variable seed1, seed2: inout positive) is
 			variable rand_val		: real;
 			variable num_requests_int	: integer;
 		begin
@@ -98,9 +100,11 @@ begin
 			num_requests := num_requests_int;
 
 			for i in 0 to (num_requests_int - 1) loop
+				uniform(seed1, seed2, rand_val);
+				req_delay(i) := integer(rand_val*real(MAX_DELAY_PER_REQUEST - 1));
 				for j in 0 to (NUM_REQ_TB - 1) loop
 					uniform(seed1, seed2, rand_val);
-					req_delay(i, j) := integer(rand_val*real(MAX_DELAY_PER_REQUEST - 1));
+					req(i, j) := rand_bool(rand_val, 0.5);
 					uniform(seed1, seed2, rand_val);
 					data_arr(i, j) := integer(rand_val*real(2.0**(real(ROW_L_TB) - 1.0)));
 					uniform(seed1, seed2, rand_val);
@@ -112,4 +116,81 @@ begin
 
 		end procedure test_param;
 
+		procedure run_arbitrer(variable num_requests_exp : in integer; variable req_delay_arr : in int_arr(0 to (MAX_REQUESTS_PER_TEST - 1)); variable data_arr, ack_delay_arr : in int_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (NUM_REQ_TB - 1)); variable req_arr, stop_arb_arr : in bool_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (NUM_REQ_TB - 1)); variable num_requests_rtl : integer; variable data_arr_exp, data_arr_rtl : in int_arr(0 to (DATA_L_TB*MAX_REQUESTS_PER_TEST - 1)); variable ack_err_arr, req_err_arr : in bool_arr(0 to (MAX_REQUESTS_PER_TEST - 1))) is
+			variable num_requests_rtl_int	: integer;
 
+			variable priority		: integer;
+
+			variable req_delay		: integer;
+			variable ack_delay		: integer;
+			variable stop_arb		: boolean;
+
+			variable ack_err		: boolean;
+			variable req_err		: boolean;
+		begin
+
+			num_requests_rtl_int := 0;
+
+			priority := 0;
+
+			data_arr_exp := reset_int_arr(0, MAX_REQUESTS_PER_TEST);
+			data_arr_rtl := reset_int_arr(0, MAX_REQUESTS_PER_TEST);
+
+			req_err_arr := reset_bool_arr(false, MAX_REQUESTS_PER_TEST);
+			ack_err_arr := reset_bool_arr(false, MAX_REQUESTS_PER_TEST);
+
+			ReqIn_tb <= (others => '0');
+			AckIn_tb <= '0';
+			StopArb <= '1';
+			DataIn <= (others => '0');
+
+			arb_loop: loop
+
+				exit arb_loop when (num_requests_rtl_int = num_requests_exp);
+
+				req_delay := req_delay_arr(num_requests_rtl_int);
+				ack_delay := ack_delay_arr(num_requests_rtl_int);
+				stop_arb := stop_arb_arr(num_requests_rtl_int);
+
+				req_err := false;
+				ack_err := false;
+
+				StopArb <= bool_to_std_logic(stop_arb);
+
+				ReqIn_tb <= (others => '0');
+
+				for i in 0 to req_delay loop
+					wait until ((clk_tb = '1') and (clk_tb'event));
+					if (i = req_delay) then
+						for j in 0 to (NUM_REQ_TB - 1) loop
+							ReqIn_tb(j) <= bool_to_std_logic(req_arr(num_requests_rtl_int, j));
+						end for;
+					else
+						if (AckOut /= ZERO_REQ_ACK_VEC) then
+							ack_err := true;
+						end if;
+						if (ReqOut = '1') then
+							req_err := true;
+						end if;
+					end if;
+				end loop;
+
+				if (ack_delay = 0) then
+					AckIn_tb <= '1';
+				else
+					for i in 0 to ack_delay loop
+						wait until ((clk_tb = '1') and (clk_tb'event));
+						if (i = ack_delay) then
+							AckIn_tb <= '1';
+						else
+							if (AckOut /= ZERO_REQ_ACK_VEC) then
+								ack_err := true;
+							end if;
+							if (ReqOut = '1') then
+								req_err := true;
+							end if;
+						end if;
+					end loop;
+				end if;
+
+				
