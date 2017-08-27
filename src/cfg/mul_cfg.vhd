134a09d469c08099e5d21c575780f7fea9d951fc
AndGate92 <mrandre.epfl@gmail.com>
2017-08-27 22:26:29 +0100

    Use booth radix 2 architecture

--- src/cfg/mul_cfg.vhd
+++ src/cfg/mul_cfg.vhd
@@ -1,7 +1,7 @@
 configuration config_mul of mul_tb is
 	for bench
 		for DUT: mul
-			use entity work.mul(booth_radix4);
+			use entity work.mul(booth_radix2);
 		end for;
 	end for;
 end config_mul;
