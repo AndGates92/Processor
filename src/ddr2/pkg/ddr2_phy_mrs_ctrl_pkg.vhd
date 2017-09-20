e961a5f6240ab98cc09f2483ce4c468fa411015e
AndGate92 <mrandre.epfl@gmail.com>
2017-09-20 21:47:28 +0100

    Add FSM state encoding of MRS ctrl

--- src/ddr2/pkg/ddr2_phy_mrs_ctrl_pkg.vhd
+++ src/ddr2/pkg/ddr2_phy_mrs_ctrl_pkg.vhd
@@ -9,11 +9,15 @@ use work.ddr2_phy_pkg.all;
 
 package ddr2_phy_mrs_ctrl_pkg is 
 
-	constant CNT_ODT_CTRL_L		: integer := int_to_bit_num(T_MOD_ns_max);
+	constant CNT_MRS_CTRL_L		: integer := int_to_bit_num(T_MOD_ns_max);
 
 	constant STATE_MRS_CTRL_L	: positive := 3;
 
-	constant MRS_CTRL_IDLE		: std_logic_vector(STATE_ODT_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(0, STATE_ODT_CTRL_L));
+	constant MRS_CTRL_IDLE			: std_logic_vector(STATE_MRS_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(0, STATE_MRS_CTRL_L));
+	constant MRS_CTRL_ODT_TURN_OFF		: std_logic_vector(STATE_MRS_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(1, STATE_MRS_CTRL_L));
+	constant MRS_CTRL_SEND_CMD		: std_logic_vector(STATE_MRS_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(2, STATE_MRS_CTRL_L));
+	constant MRS_CTRL_REG_UPD		: std_logic_vector(STATE_MRS_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(3, STATE_MRS_CTRL_L));
+	constant MRS_CTRL_ODT_TURN_ON		: std_logic_vector(STATE_MRS_CTRL_L - 1 downto 0) := std_logic_vector(to_unsigned(4, STATE_MRS_CTRL_L));
 
 	component ddr2_phy_mrs_ctrl is
 	--generic (
@@ -41,7 +45,7 @@ package ddr2_phy_mrs_ctrl_pkg is
 
 		ODTCtrlReq		: out std_logic;
 
-		-- Stop Arbiter
+		-- Turn ODT signal on after MRS command(s)
 		MRSUpdateCompleted	: out std_logic
 	);
 	end component;
