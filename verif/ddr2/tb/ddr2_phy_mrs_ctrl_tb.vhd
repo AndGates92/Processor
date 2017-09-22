16ac57607d7a0017cce1f2c40f1e2e8093927544
AndGate92 <mrandre.epfl@gmail.com>
2017-09-22 21:11:57 +0100

    Instantiate DUT and add reset and test_param procedures

--- verif/ddr2/tb/ddr2_phy_mrs_ctrl_tb.vhd
+++ verif/ddr2/tb/ddr2_phy_mrs_ctrl_tb.vhd
@@ -24,7 +24,8 @@ architecture bench of ddr2_phy_odt_ctrl_tb is
 	constant TOT_NUM_TESTS	: integer := NUM_TESTS;
 
 	constant MAX_REQUESTS_PER_TEST	: integer := 500;
-	constant MAX_BURST_DELAY	: integer := 20;
+	constant MAX_CMD_PER_REQUESTS	: integer := 500;
+	constant MAX_DELAY		: integer := 20;
 
 	constant MRS_REG_L_TB	: positive := 13;
 
@@ -54,4 +55,113 @@ architecture bench of ddr2_phy_odt_ctrl_tb is
 	-- Turn ODT signal on after MRS command(s)
 	signal MRSUpdateCompleted_tb	: std_logic;
 
+begin
 
+	DUT: ddr2_phy_odt_ctrl generic map (
+		MRS_REG_L => MRS_REG_L_TB
+	)
+	port map (
+		clk => clk_tb,
+		rst => rst_tb,
+
+		-- Transaction Controller
+		CtrlReq => CtrlReq_tb,
+		CtrlCmd => CtrlCmd_tb,
+		CtrlData => CtrlData_tb,
+
+		CtrlAck => CtrlAck_tb,
+
+		-- Commands
+		CmdAck => CmdAck_tb,
+
+		CmdReq => CmdReq_tb,
+		Cmd => Cmd_tb,
+		Data => Data_tb,
+
+		-- ODT Controller
+		ODTCtrlAck => ODTCtrlAck_tb,
+
+		ODTCtrlReq => ODTCtrlReq_tb,
+
+		-- Turn ODT signal on after MRS command(s)
+		MRSUpdateCompleted => MRSUpdateCompleted_tb
+	);
+
+	clk_gen(CLK_PERIOD, 0 ns, stop, clk_tb);
+
+	test: process
+
+		procedure reset is
+		begin
+
+			-- Transaction Controller
+			CtrlReq_tb <= '0';
+			CtrlCmd_tb <= CMD_NOP;
+			CtrlData_tb <= (others => '0');
+
+			-- Commands
+			CmdAck_tb <= '0';
+
+			-- ODT Controller
+			ODTCtrlAck_tb <= '0';
+
+			rst_tb <= '0';
+			wait until ((clk_tb'event) and (clk_tb = '1'));
+			rst_tb <= '1';
+			wait until ((clk_tb'event) and (clk_tb = '1'));
+			rst_tb <= '0';
+		end procedure reset;
+
+		procedure test_param(variable num_requests : out integer; variable num_cmd_per_request  : out int_arr(0 to (MAX_REQUESTS_PER_TEST - 1)); variable ctrl_cmd, ctrl_data, ctrl_delay, odt_delay, cmd_delay : out int_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (MAX_CMD_PER_REQUEST - 1)); variable seed1, seed2: inout positive) is
+			variable rand_val	: real;
+			variable num_requests_int	: integer;
+			variable num_cmd_per_request_int	: integer;
+		begin
+			num_requests_int := 0;
+			while (num_requests_int = 0) loop
+				uniform(seed1, seed2, rand_val);
+				num_requests_int := integer(rand_val*real(MAX_REQUESTS_PER_TEST));
+			end loop;
+			num_requests := num_requests_int;
+
+			for i in 0 to (num_requests_int - 1) loop
+				num_cmd_per_request_int := 0;
+				while (num_cmd_per_request_int = 0) loop
+					uniform(seed1, seed2, rand_val);
+					num_cmd_per_request_int := integer(rand_val*real(MAX_CMD_PER_REQUEST));
+				end loop;
+				num_cmd_per_request(i) := num_cmd_per_request_int;
+
+				for j in 0 to (num_cmd_per_request_int - 1) loop
+					uniform(seed1, seed2, rand_val);
+					ctrl_delay(i, j) := integer(rand_val*real(MAX_DELAY));
+					uniform(seed1, seed2, rand_val);
+					odt_delay(i, j) := integer(rand_val*real(MAX_DELAY));
+					uniform(seed1, seed2, rand_val);
+					cmd_delay(i, j) := integer(rand_val*real(MAX_DELAY));
+					uniform(seed1, seed2, rand_val);
+					ctrl_cmd(i, j) := integer(rand_val*real(MAX_MEM_CMD_ID));
+					uniform(seed1, seed2, rand_val);
+					ctrl_data(i, j) := integer(rand_val*real(2.0**(real(MRS_REG_L_TB))));
+				end loop;
+
+				for j in num_cmd_per_request_int to (MAX_CMD_PER_REQUEST - 1) loop
+					ctrl_delay(i, j) := 0;
+					odt_delay(i, j) := 0;
+					cmd_delay(i, j) := 0;
+					ctrl_cmd(i, j) := CMD_NOP;
+					ctrl_data(i, j) := 0;
+				end loop;
+			end loop;
+
+			for i in num_requests_int to (MAX_REQUESTS_PER_TEST - 1) loop
+				for j in 0 to (MAX_CMD_PER_REQUEST - 1) loop
+					ctrl_delay(i, j) := 0;
+					odt_delay(i, j) := 0;
+					cmd_delay(i, j) := 0;
+					ctrl_cmd(i, j) := CMD_NOP;
+					ctrl_data(i, j) := 0;
+				end loop;
+			end loop;
+
+		end procedure test_param;
