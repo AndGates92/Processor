a6c50a0660b26bd4c87e336436523b97f482f06b
AndGate92 <mrandre.epfl@gmail.com>
2017-09-24 18:58:41 +0100

    Add extra tests to icrease coverage

--- verif/ddr2/tb/ddr2_phy_mrs_ctrl_tb.vhd
+++ verif/ddr2/tb/ddr2_phy_mrs_ctrl_tb.vhd
@@ -20,9 +20,10 @@ end entity ddr2_phy_mrs_ctrl_tb;
 
 architecture bench of ddr2_phy_mrs_ctrl_tb is
 
-	constant CLK_PERIOD	: time := DDR2_CLK_PERIOD * 1 ns;
-	constant NUM_TESTS	: integer := 10000;
-	constant TOT_NUM_TESTS	: integer := NUM_TESTS;
+	constant CLK_PERIOD		: time := DDR2_CLK_PERIOD * 1 ns;
+	constant NUM_TESTS		: integer := 1000;
+	constant NUM_EXTRA_TESTS	: integer := 16;
+	constant TOT_NUM_TESTS		: integer := NUM_TESTS + NUM_EXTRA_TESTS;
 
 	constant MAX_REQUESTS_PER_TEST	: integer := 50;
 	constant MAX_CMD_PER_REQUEST	: integer := 50;
@@ -112,6 +113,73 @@ begin
 			wait until ((clk_tb'event) and (clk_tb = '1'));
 			rst_tb <= '0';
 		end procedure reset;
+
+		procedure setup_extra_tests(variable rand_odt_delay, rand_ctrl_delay, rand_cmd_delay : in boolean; variable num_requests : out integer; variable num_cmd_per_request : out int_arr(0 to (MAX_REQUESTS_PER_TEST - 1)); variable ctrl_cmd, ctrl_data, ctrl_delay, cmd_delay : out int_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (MAX_CMD_PER_REQUEST - 1)); variable odt_delay : out int_arr(0 to (MAX_REQUESTS_PER_TEST - 1)); variable seed1, seed2: inout positive) is
+			variable rand_val	: real;
+			variable num_requests_int	: integer;
+			variable num_cmd_per_request_int	: integer;
+		begin
+			num_requests_int := 0;
+			while (num_requests_int = 0) loop
+				uniform(seed1, seed2, rand_val);
+				num_requests_int := integer(rand_val*real(MAX_REQUESTS_PER_TEST));
+			end loop;
+			num_requests := num_requests_int;
+
+			for i in 0 to (num_requests_int - 1) loop
+				num_cmd_per_request_int := 0;
+				while (num_cmd_per_request_int = 0) loop
+					uniform(seed1, seed2, rand_val);
+					num_cmd_per_request_int := integer(rand_val*real(MAX_CMD_PER_REQUEST));
+				end loop;
+				num_cmd_per_request(i) := num_cmd_per_request_int;
+
+				if (rand_odt_delay = true) then
+					odt_delay(i) := 0;
+				else
+					uniform(seed1, seed2, rand_val);
+					odt_delay(i) := integer(rand_val*real(MAX_DELAY));
+				end if;
+
+				for j in 0 to (num_cmd_per_request_int - 1) loop
+					if (rand_ctrl_delay = true) then
+						ctrl_delay(i, j) := 0;
+					else
+						uniform(seed1, seed2, rand_val);
+						ctrl_delay(i, j) := integer(rand_val*real(MAX_DELAY));
+					end if;
+					if (rand_cmd_delay = true) then
+						cmd_delay(i, j) := 0;
+					else
+						uniform(seed1, seed2, rand_val);
+						cmd_delay(i, j) := integer(rand_val*real(MAX_DELAY));
+					end if;
+					uniform(seed1, seed2, rand_val);
+					ctrl_cmd(i, j) := integer(rand_val*real(MAX_MEM_CMD_ID));
+					uniform(seed1, seed2, rand_val);
+					ctrl_data(i, j) := integer(rand_val*real((2.0**(real(MRS_REG_L_TB))) - 1.0));
+				end loop;
+
+				for j in num_cmd_per_request_int to (MAX_CMD_PER_REQUEST - 1) loop
+					ctrl_delay(i, j) := 0;
+					cmd_delay(i, j) := 0;
+					ctrl_cmd(i, j) := to_integer(unsigned(CMD_NOP));
+					ctrl_data(i, j) := 0;
+				end loop;
+			end loop;
+
+			for i in num_requests_int to (MAX_REQUESTS_PER_TEST - 1) loop
+				num_cmd_per_request(i) := 0;
+				odt_delay(i) := 0;
+				for j in 0 to (MAX_CMD_PER_REQUEST - 1) loop
+					ctrl_delay(i, j) := 0;
+					cmd_delay(i, j) := 0;
+					ctrl_cmd(i, j) := to_integer(unsigned(CMD_NOP));
+					ctrl_data(i, j) := 0;
+				end loop;
+			end loop;
+
+		end procedure setup_extra_tests;
 
 		procedure test_param(variable num_requests : out integer; variable num_cmd_per_request : out int_arr(0 to (MAX_REQUESTS_PER_TEST - 1)); variable ctrl_cmd, ctrl_data, ctrl_delay, cmd_delay : out int_arr_2d(0 to (MAX_REQUESTS_PER_TEST - 1), 0 to (MAX_CMD_PER_REQUEST - 1)); variable odt_delay : out int_arr(0 to (MAX_REQUESTS_PER_TEST - 1)); variable seed1, seed2: inout positive) is
 			variable rand_val	: real;
@@ -576,7 +644,6 @@ begin
 		variable num_requests_exp	: integer;
 		variable num_requests_rtl	: integer;
 
-
 		variable num_cmd_per_request_arr_exp	: int_arr(0 to (MAX_REQUESTS_PER_TEST - 1));
 		variable num_cmd_per_request_arr_rtl	: int_arr(0 to (MAX_REQUESTS_PER_TEST - 1));
 
@@ -590,6 +657,10 @@ begin
 		variable odt_delay_arr		: int_arr(0 to (MAX_REQUESTS_PER_TEST - 1));
 
 		variable mrs_ctrl_err_arr	: int_arr(0 to (MAX_REQUESTS_PER_TEST - 1));
+
+		variable rand_odt_delay		: boolean;
+		variable rand_cmd_delay		: boolean;
+		variable rand_ctrl_delay	: boolean;
 
 		variable pass	: integer;
 		variable num_pass	: integer;
@@ -622,6 +693,59 @@ begin
 			wait until ((clk_tb'event) and (clk_tb = '1'));
 
 		end loop;
+
+		if (NUM_EXTRA_TESTS > 0) then
+
+			for i in 0 to NUM_EXTRA_TESTS-1 loop
+
+				if ((i mod 8) = 0) then
+					rand_odt_delay := false;
+					rand_cmd_delay := false;
+					rand_ctrl_delay := false;
+				elsif ((i mod 8) = 1) then
+					rand_odt_delay := true;
+					rand_cmd_delay := false;
+					rand_ctrl_delay := false;
+				elsif ((i mod 8) = 2) then
+					rand_odt_delay := false;
+					rand_cmd_delay := true;
+					rand_ctrl_delay := false;
+				elsif ((i mod 8) = 3) then
+					rand_odt_delay := true;
+					rand_cmd_delay := true;
+					rand_ctrl_delay := false;
+				elsif ((i mod 8) = 4) then
+					rand_odt_delay := false;
+					rand_cmd_delay := false;
+					rand_ctrl_delay := true;
+				elsif ((i mod 8) = 5) then
+					rand_odt_delay := true;
+					rand_cmd_delay := false;
+					rand_ctrl_delay := true;
+				elsif ((i mod 8) = 6) then
+					rand_odt_delay := false;
+					rand_cmd_delay := true;
+					rand_ctrl_delay := true;
+				else
+					rand_odt_delay := false;
+					rand_cmd_delay := true;
+					rand_ctrl_delay := true;
+				end if;
+
+				setup_extra_tests(rand_odt_delay, rand_ctrl_delay, rand_cmd_delay, num_requests_exp, num_cmd_per_request_arr_exp, ctrl_cmd_arr_exp, ctrl_data_arr_exp, ctrl_delay_arr, cmd_delay_arr, odt_delay_arr, seed1, seed2);
+
+				run_mrs_ctrl(num_requests_exp, num_cmd_per_request_arr_exp, ctrl_cmd_arr_exp, ctrl_data_arr_exp, ctrl_delay_arr, cmd_delay_arr, odt_delay_arr, num_requests_rtl, num_cmd_per_request_arr_rtl, mrs_ctrl_err_arr, ctrl_cmd_arr_rtl, ctrl_data_arr_rtl);
+
+				verify(num_requests_exp, num_requests_rtl, num_cmd_per_request_arr_exp, num_cmd_per_request_arr_rtl, ctrl_cmd_arr_exp, ctrl_data_arr_exp, ctrl_cmd_arr_rtl, ctrl_data_arr_rtl, mrs_ctrl_err_arr, file_pointer, pass);
+
+				num_pass := num_pass + pass;
+
+				wait until ((clk_tb'event) and (clk_tb = '1'));
+
+
+			end loop;
+
+		end if;
 
 		file_close(file_pointer);
 
