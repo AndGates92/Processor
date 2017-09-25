23d04a7c2fcae43239c5713d013731cc71c61fa1
AndGate92 <mrandre.epfl@gmail.com>
2017-09-25 22:51:18 +0100

    Use new pkgs and rename data width

--- verif/common/tb/arbiter_tb.vhd
+++ verif/common/tb/arbiter_tb.vhd
@@ -6,22 +6,24 @@ use ieee.math_real.all;
 use std.textio.all;
 
 library work;
-use work.proc_pkg.all;
+use work.shared_tb_pkg.all;
+use work.common_tb_pkg.all;
+use work.functions_tb_pkg.all;
+use work.common_log_pkg.all;
 use work.arbiter_pkg.all;
 use work.type_conversion_pkg.all;
-use work.tb_pkg.all;
 
 entity arbiter_tb is
 end entity arbiter_tb;
 
 architecture bench of arbiter_tb is
 
-	constant CLK_PERIOD	: time := PROC_CLK_PERIOD * 1 ns;
+	constant CLK_PERIOD	: time := COMMON_CLK_PERIOD * 1 ns;
 	constant NUM_TESTS	: integer := 1000;
 	constant TOT_NUM_TESTS	: integer := NUM_TESTS;
 
 	constant NUM_REQ_TB	: integer := 8;
-	constant DATA_L_TB	: integer := 32;
+	constant ARB_DATA_L_TB	: integer := 32;
 
 	constant MAX_REQUESTS_PER_TEST		: integer := 500;
 	constant MAX_DELAY_PER_REQUEST		: integer := 20;
@@ -36,13 +38,13 @@ architecture bench of arbiter_tb is
 
 	-- Request Set
 	signal ReqIn_tb		: std_logic_vector(NUM_REQ_TB - 1 downto 0);
-	signal DataIn_tb	: std_logic_vector(DATA_L_TB*NUM_REQ_TB - 1 downto 0);
+	signal DataIn_tb	: std_logic_vector(ARB_DATA_L_TB*NUM_REQ_TB - 1 downto 0);
 
 	signal AckIn_tb		: std_logic;
 
 	-- Request
 	signal ReqOut_tb	: std_logic;
-	signal DataOut_tb	: std_logic_vector(DATA_L_TB - 1 downto 0);
+	signal DataOut_tb	: std_logic_vector(ARB_DATA_L_TB - 1 downto 0);
 
 	signal AckOut_tb	: std_logic_vector(NUM_REQ_TB - 1 downto 0);
 
@@ -53,7 +55,7 @@ begin
 
 	DUT: arbiter generic map (
 		NUM_REQ => NUM_REQ_TB,
-		DATA_L => DATA_L_TB
+		DATA_L => ARB_DATA_L_TB
 	)
 	port map (
 		clk => clk_tb,
@@ -113,7 +115,7 @@ begin
 					uniform(seed1, seed2, rand_val);
 					req(i, j) := rand_bool(rand_val, 0.5);
 					uniform(seed1, seed2, rand_val);
-					data_arr(i, j) := integer(rand_val*real(2.0**(real(DATA_L_TB) - 1.0)));
+					data_arr(i, j) := integer(rand_val*real(2.0**(real(ARB_DATA_L_TB) - 1.0)));
 					uniform(seed1, seed2, rand_val);
 					ack_delay(i, j) := integer(rand_val*real(MAX_DELAY_PER_REQUEST - 1));
 				end loop;
@@ -179,7 +181,7 @@ begin
 
 						for j in 0 to (NUM_REQ_TB - 1) loop
 							ReqIn_tb(j) <= bool_to_std_logic(req_arr_int(num_requests_rtl_int, j));
-							DataIn_tb((j+1)*DATA_L_TB - 1 downto j*DATA_L_TB) <= std_logic_vector(to_unsigned(data_arr(num_requests_rtl_int, j), DATA_L_TB));
+							DataIn_tb((j+1)*ARB_DATA_L_TB - 1 downto j*ARB_DATA_L_TB) <= std_logic_vector(to_unsigned(data_arr(num_requests_rtl_int, j), ARB_DATA_L_TB));
 						end loop;
 					else
 						if (AckOut_tb /= ZERO_REQ_ACK_VEC) then
