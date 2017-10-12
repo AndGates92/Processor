0a5e1e72afcf5c2fede0b7f086239e4cfc76aa0b
AndGate92 <mrandre.epfl@gmail.com>
2017-10-12 23:03:06 +0100

    change RTL entity name: execute_state -> execute

--- verif/cpu/cfg/execute_tb_cfg.vhd
+++ verif/cpu/cfg/execute_tb_cfg.vhd
@@ -3,7 +3,7 @@ library cpu_rtl;
 
 configuration config_execute_tb of execute_tb is
 	for bench
-		for DUT: execute_stage
+		for DUT: execute
 			use configuration cpu_rtl.config_execute;
 		end for;
 	end for;
