dbf3d796f83429be73e0e7694e0e18db922048ad
AndGate92 <mrandre.epfl@gmail.com>
2017-10-18 22:05:59 +0100

    Create four_act_win_unsigned and counter widths

--- src/ddr2/pkg/ddr2_phy_arbiter_ctrl_pkg.vhd
+++ src/ddr2/pkg/ddr2_phy_arbiter_ctrl_pkg.vhd
@@ -6,8 +6,10 @@ use ieee.numeric_std.all;
 package ddr2_phy_arbiter_ctrl is
 
 	constant WINDOW_L		: integer := 4;
-	constant CNT_ARR_ARB_CTRL_L	: integer := int_to_bit_num(max_int(T_FAW_min, T_RRD));
+	constant CNT_ARR_FOUR_ACT_WIN_L	: integer := int_to_bit_num(T_FAW_min);
+	constant CNT_ACT_TO_ACT_L	: integer := int_to_bit_num(T_RRD);
 
-	type arb_ctrl_unsigned_arr is array (integer range <>) of unsigned((CNT_ARR_ARB_CTRL_L - 1) downto 0);
+	type four_act_win_unsigned is unsigned((CNT_ARR_FOUR_ACT_WIN_L - 1) downto 0);
+	type four_act_win_unsigned_arr is array (integer range <>) of four_act_win_unsigned;
 
 end package ddr2_phy_arbiter_ctrl;
