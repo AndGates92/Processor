6bd8c6a34edd170d9fde8bb32f26f3fd058a6010
AndGate92 <mrandre.epfl@gmail.com>
2017-10-19 22:30:28 +0100

    Complete arbiter ctrl architecture: tRRD cnt and AllowBankAct signal logic

--- src/ddr2/rtl/ddr2_phy_arbiter_ctrl.vhd
+++ src/ddr2/rtl/ddr2_phy_arbiter_ctrl.vhd
@@ -31,6 +31,8 @@ architecture rtl of ddr2_phy_arbiter_ctrl is
 	constant zero_four_act_win_cnt					: unsigned(four_act_win_unsigned'length - 1 downto 0) := (others => '0');
 	constant incr_four_act_win_cnt_value				: unsigned(four_act_win_unsigned'length - 1 downto 0) := to_unsigned(1, four_act_win_unsigned'length);
 
+	constant incr_four_act_win_ptr_value				: unsigned(int_to_bit_num(WINDOW_L) - 1 downto 0) := to_unsigned(1, int_to_bit_num(WINDOW_L));
+
 	constant zero_act_to_act_cnt					: unsigned(CNT_ACT_TO_ACT_L - 1 downto 0) := (others => '0');
 	constant incr_act_to_act_cnt_value				: unsigned(CNT_ACT_TO_ACT_L - 1 downto 0) := to_unsigned(1, CNT_ACT_TO_ACT_L);
 
@@ -52,7 +54,32 @@ architecture rtl of ddr2_phy_arbiter_ctrl is
 
 	signal AllowBankActivate_int					: std_logic;
 
+	signal FourActWinElapsed					: std_logic;
+
 begin
+
+	reg: process(rst, clk)
+	begin
+		if (rst = '1') then
+			CntFourActWinArrC <= (others => (others => '0'));
+			FourActWinCntArrEnC <= (others => '0');
+
+			CntFourActWinNextActPtrC <= (others => '0');
+			CntFourActWinLastActPtrC <= (others => '0');
+
+			CntActToActC <= (others => '0');
+			ActToActCntEnC <= (others => '0');
+		elsif ((clk'event) and (clk = '1')) then
+			CntFourActWinArrC <= CntFourActWinArrN;
+			FourActWinCntArrEnC <= FourActWinCntArrEnN;
+
+			CntFourActWinNextActPtrC <= CntFourActWinNextActPtrN;
+			CntFourActWinLastActPtrC <= CntFourActWinLastActPtrN;
+
+			CntActToActC <= CntActToActN;
+			ActToActCntEnC <= ActToActCntEnN;
+		end if;
+	end process reg;
 
 	-- Pause arbiter when updating MRS registers (ODT controller sends the request because ODT is taken low)
 	PauseArbiter <= ODTCtrlPauseArbiter;
@@ -67,19 +94,47 @@ begin
 	end generate ZERO_FOUR_ACT_CNT;
 
 	FOUR_ACT_CNT_EN: for i in 0 to (WINDOW_L - 1) generate
-		FourActWinCntArrEnN(i) <= '1' when (CntFourActWinNextActPtr = to_unsigned(i, int_to_bit_num(WINDOW_L))) else FourActWinCntArrEnC;
+		FourActWinCntArrEnN(i) <= '1' when (CntFourActWinNextActPtrC = to_unsigned(i, int_to_bit_num(WINDOW_L))) else FourActWinCntArrEnC;
 	end generate FOUR_ACT_CNT_EN;
 
 	SET_FOUR_ACT_CNT: for i in 0 to (WINDOW_L - 1) generate
-		SetFourActWinCntArr(i) <= BankActCmd when (CntFourActWinNextActPtr = to_unsigned(i, int_to_bit_num(WINDOW_L))) else '0';
+		SetFourActWinCntArr(i) <= BankActCmd when (CntFourActWinNextActPtrC = to_unsigned(i, int_to_bit_num(WINDOW_L))) else '0';
 	end generate SET_FOUR_ACT_CNT;
 
 	FOUR_ACT_CNT: for i in 0 to (WINDOW_L - 1) generate
 		CntFourActWinArrN(i) <=	FourActWinCntInitValue					when (SetFourActWinCntArr(i) = '1') else
-					(CntFourActWinArrC(i) + incr_four_act_win_cnt_value)	when ((FourActWinCntArrEnC(i) = '1') and (ZeroFourActWinCntArr(i) = '0')) else
+					(CntFourActWinArrC(i) - incr_four_act_win_cnt_value)	when ((FourActWinCntArrEnC(i) = '1') and (ZeroFourActWinCntArr(i) = '0')) else
 					CntFourActWinArrC(i);
 	end generate FOUR_ACT_CNT;
 
+	tFAW_mux: process(ZeroFourActWinCntArr, CntFourActWinLastActPtrC)
+	begin
+		FourActWinElapsed <= '0';
+		for i in 0 to (WINDOW_L - 1) loop
+			if (CntFourActWinLastActPtrC = to_unsigned(i, int_to_bit_num(WINDOW_L))) begin
+				FourActWinElapsed <= ZeroFourActWinCntArr(i);
+			end if;
+		end loop;
+	end process tFAW_mux;
 
+	IncrCntFourActWinPtr <= BankActCmd;
+
+	CntFourActWinLastActPtrN <= (CntFourActWinLastActPtrC + incr_four_act_win_ptr_value) when (IncrCntFourActWinPtr = '1') else CntFourActWinLastActPtrC;
+	CntFourActWinNextActPtrN <= (CntFourActWinNextActPtrC + incr_four_act_win_ptr_value) when (IncrCntFourActWinPtr = '1') else CntFourActWinNextActPtrC;
+
+	-- Activate to Activate counter initial value. tRRD - 1 because count down to 0
+	ActToActCntInitValue <= to_unsigned((T_RRD - 1), CNT_ACT_TO_ACT_L);
+
+	SetActToActCnt <= BankActCmd;
+
+	ActToActCntEnN <= '1' when SetActToActCnt else ActToActCntEnC;
+
+	ZeroActToActCnt <= '1' when (CntActToActC(i) = zero_act_to_act_cnt) else '0';
+
+	CntActToActN(i) <=	ActToActCntInitValue					when (SetActToActCnt(i) = '1') else
+				(CntActToActC(i) - incr_act_to_act_cnt_value)		when ((ActToActCntEnC(i) = '1') and (ZeroActToActCnt(i) = '0')) else
+				CntActToActC(i);
+
+	AllowBankActivate_int <= FourActWinElapsed and ZeroActToActCnt;
 
 end rtl;
